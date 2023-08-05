55801957421f1ae265044956a28f811408f9e52a
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-08-05 15:57:12 -0300

    Studing type script

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
+import { Observable, Observer, Subject } from "rxjs";
+function recieveEvents(observable: Observable<string>) {
+    observable.subscribe({
+        next: str => {
+            console.log(`Event received: ${str}`);
+        },
+        complete: () => console.log("Sequence ended")
+    });
 }
+function sendEvents(observer: Observer<string>) {
+    let count = 5;
+    for (let i = 0; i < count; i++) {
+        observer.next(`${i + 1} of ${count}`);
+    }
+    observer.complete();
+}
+let subject = new Subject<string>();
+recieveEvents(subject);
+sendEvents(subject);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
